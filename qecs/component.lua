97fb8229a3b9776b16b4fcd18f440b5a0fb89202
Justin van der Leij <justinvanderleij@gmail.com>
2018-01-16 11:44:03 +0100

    Added components inheritance

--- qecs/component.lua
+++ qecs/component.lua
@@ -1,28 +1,33 @@
 local Component = {}
 Component.__index = Component
 
-function Component.new(populate)
+function Component.new(populate, inherit)
    local component = setmetatable({
-      populate = populate,
+      __populate = populate,
+      __inherit  = inherit,
    }, Component)
+
+   if inherit then
+      component.__mt = {__index = component}
+   end
 
    return component
 end
 
 function Component:initialize(e, ...)
-   if self.populate then
+   if self.__populate then
       local bag = {}
-      self.populate(bag, ...)
+      self.__populate(bag, ...)
+
+      if self.__inherit then
+         setmetatable(bag, self.__mt)
+      end
+
       return bag
    end
 
    return true
 end
-
---[[
-function Component.populate(e, ...)
-end
-]]
 
 return setmetatable(Component, {
    __call = function(_, ...) return Component.new(...) end,
